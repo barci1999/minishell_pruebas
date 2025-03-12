4ea3b2516d2cab62ccdd55314b9714b5c8f9f2b3
barci1999 <pabloalvarezbar@gmail.com>
2025-03-12 20:40:16 +0100

    enums

--- include/pruebas.h
+++ include/pruebas.h
@@ -8,6 +8,52 @@
 # include"libft.h"
 # include "ft_printf.h"
 
+typedef enum e_token_type {
+    CMD,        // Comando (por ejemplo, 'ls', 'cat', etc.)
+    ARG,        // Argumento de un comando (por ejemplo, 'archivo.txt', 'directorio', etc.)
+    PIPE,       // Operador pipe '|'
+    REDIR_IN,   // Redirección de entrada '<'
+    REDIR_OUT,  // Redirección de salida '>'
+    REDIR_APPEND, // Redirección de salida en modo append '>>'
+    HEREDOC,    // Redirección de entrada con delimitador '<<'
+    ENV_VAR,    // Variables de entorno, por ejemplo '$HOME'
+    DOLLAR_EXIT, // Expansión de $? para el estado de salida
+    QUOTE,      // Comillas simples (') o dobles (")
+    INVALID     // Token inválido o no reconocido
+} t_token_type;
+
+typedef enum e_signal_type {
+    SIG_INT,    // CTRL+C (Interrupción)
+    SIG_QUIT,   // CTRL+\ (Quit)
+    SIG_EOF,    // CTRL+D (Fin de archivo)
+    SIG_NONE    // Sin señal
+} t_signal_type;
+
+typedef enum e_builtin_type {
+    ECHO,       // Comando echo con opción -n
+    CD,         // Comando cd con rutas absolutas o relativas
+    PWD,        // Comando pwd
+    EXPORT,     // Comando export
+    UNSET,      // Comando unset
+    ENV,        // Comando env
+    EXIT,       // Comando exit
+    BUILTIN_NONE // No es un comando interno
+} t_builtin_type;
+
+typedef enum e_exec_status {
+    EXEC_SUCCESS,  // Ejecución exitosa
+    EXEC_FAILURE,  // Fallo en la ejecución
+    EXEC_RUNNING,  // Comando en ejecución
+    EXEC_EXIT      // Terminación del shell (exit)
+} t_exec_status;
+
+typedef enum e_expansion_type {
+    EXPAND_NONE,       // No expansión
+    EXPAND_ENV_VAR,    // Expansión de variable de entorno (por ejemplo, $HOME)
+    EXPAND_EXIT_STATUS, // Expansión de $? (estado de salida)
+    EXPAND_UNKNOWN     // Expansión desconocida o no válida
+} t_expansion_type;
+
 typedef struct t_list
 {
 	void			*content;
@@ -19,5 +65,6 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void parse_split(char **matrix);
 t_list	*ft_lstlast(t_list *lst);
-
+t_token_type classify_tokken(char *token);
+void tonkenize(char *input);
 #endif
--- src/main.c
+++ src/main.c
@@ -1,58 +1,101 @@
-#include"pruebas.h"
+#include "pruebas.h"
 
-int is_cmd(char *cmd)
+int	is_cmd(char *cmd)
 {
-	int i = 0;
-	char **path = ft_split(getenv("PATH"),':');
-	char *temp = NULL;
+	int		i;
+	char	**path;
+	char	*temp;
+
+	i = 0;
+	if(access(cmd,X_OK) == 0)
+		return(1);
+	path = ft_split(getenv("PATH"), ':');
+	temp = NULL;
 	while (path[i] != NULL)
 	{
-
-		temp = ft_strjoin(path[i],"/");
-			free(path[i]);
-		path[i] = ft_strjoin(temp,cmd);
-		if(access(path[i],X_OK) == 0)
-		{
-			free(path[i]);
-			free(temp);
-			return(1);
-		}
-		else
-		{
-			free(path[i]);
-			i++;
-		}
-	}
-	free(path);
-	return(0);
-}
-void parse_split(char **matrix)
-{
-	int i = 0;
-	t_list *list;
-	list = NULL;
-
-	while (matrix[i] != NULL)
-	{
-		if(is_cmd(matrix[i]))
-			printf("%s es un comando\n",matrix[i]);	
+		temp = ft_get_strjoin(path[i], "/");
+		path[i] = ft_get_strjoin(temp, cmd);
+		if (access(path[i], X_OK) == 0)
+			return (ft_free_matrix(path),1);
 		i++;
 	}
+	return (ft_free_matrix(path),0);
 }
-int main(int argc,char **argv,char **envp)
+t_token_type	classify_tokken(char *token)
+{
+	if (ft_strcmp(token, "|") == 0)
+		return (PIPE);
+	else if (ft_strcmp(token, "<") == 0)
+		return (REDIR_IN);
+	else if (ft_strcmp(token, ">") == 0)
+		return (REDIR_OUT);
+	else if (ft_strcmp(token, ">>") == 0)
+		return (REDIR_APPEND);
+	else if (ft_strcmp(token, "<<") == 0)
+		return (HEREDOC);
+	else if (token[0] == '$')
+	{
+		if (ft_strcmp(token, "$?") == 0)
+			return (DOLLAR_EXIT);
+		return (ENV_VAR);
+	}
+	else if (is_cmd(token))
+		return (CMD);
+	else
+		return (INVALID);
+}
+
+void	tonkenize(char *input)
+{
+	char			**tokens;
+	int				i;
+	t_token_type	token_type;
+
+	tokens = ft_split(input, ' ');
+	i = 0;
+	while (tokens[i])
+	{
+		token_type = classify_tokken(tokens[i]);
+		if (token_type == CMD)
+			printf("CMD: %s\n", tokens[i]);
+		else if (token_type == ARG)
+			printf("ARG: %s\n", tokens[i]);
+		else if (token_type == PIPE)
+			printf("PIPE: %s\n", tokens[i]);
+		else if (token_type == REDIR_IN)
+			printf("REDIR_IN: %s\n", tokens[i]);
+		else if (token_type == REDIR_OUT)
+			printf("REDIR_OUT: %s\n", tokens[i]);
+		else if (token_type == REDIR_APPEND)
+			printf("REDIR_APPEND: %s\n", tokens[i]);
+		else if (token_type == HEREDOC)
+			printf("HEREDOC: %s\n", tokens[i]);
+		else if (token_type == ENV_VAR)
+			printf("ENV_VAR: %s\n", tokens[i]);
+		else if (token_type == DOLLAR_EXIT)
+			printf("DOLLAR_EXIT: %s\n", tokens[i]);
+		else if (token_type == QUOTE)
+			printf("QUOTE: %s\n", tokens[i]);
+		else if (token_type == INVALID)
+			printf("INVALID: %s\n", tokens[i]);
+		i++;
+	}
+	ft_free_matrix(tokens);
+}
+int	main(int argc, char **argv, char **envp)
 {
 	char *input;
 	(void)argv;
 	(void)envp;
-	if(argc < 1)
-		return(1);
-	while(1)
+	if (argc < 1)
+		return (1);
+	while (1)
 	{
 		input = readline("minishell> ");
-		if(input == NULL)
-			break;
-		if(*input != '\0')
-			parse_split(ft_split(input,' '));
+		if (input == NULL)
+			break ;
+		if (*input != '\0')
+			tonkenize(input);
 	}
-	return(0);
+	return (0);
 }
